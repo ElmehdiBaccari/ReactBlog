98eda6e56e6681966c1b59082a7432f6406904bc
ElmehdiBaccari <60005420+ElmehdiBaccari@users.noreply.github.com>
2022-01-06 09:40:08 +0100

    Components Architecture

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import PostList from './post/PostList'
+import CreatePost from './post/CreatePost'
+import UserBar from './user/UserBar'
+
+const defaultPosts = [
+    { title: 'React Hooks', content: 'The greatest thing since sliced bread!', author: 'Daniel Bugl' },
+    { title: 'Using React Fragments', content: 'Keeping the DOM tree clean!', author: 'Daniel Bugl' }
+]
+
+export default function App () {
+    const [ user, setUser ] = useState('')
+    const [ posts, setPosts ] = useState(defaultPosts)
+
+    return (
+        <div style={{ padding: 8 }}>
+            <UserBar user={user} setUser={setUser} />
+            <br />
+            {user && <CreatePost user={user} posts={posts} setPosts={setPosts} />}
+            <br />
+            <hr />
+            <PostList posts={posts} />
+        </div>
+    )
 }
 
-export default App;
+
